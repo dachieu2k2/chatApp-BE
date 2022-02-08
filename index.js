a667a20297c86e474223630fbc758834c77cae05
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-08 21:34:50 +0700

    connectDB

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.occ7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+    console.log(`connected DB`);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 const app = express();
 
