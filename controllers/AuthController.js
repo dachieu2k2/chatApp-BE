d9a2eead8b9f90acb54784549624581635e925e3
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-08 22:50:39 +0700

    register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const AuthController = {
   loginUser: async (req, res) => {
@@ -7,8 +8,36 @@ const AuthController = {
       res.json({ success: false, message: "sign the username or password" });
     }
     try {
+      const FoundUser = User.findOne({ username });
+      if (FoundUser)
+        res.json({ success: false, message: "please choose another name" });
     } catch (error) {}
     console.log(req.body);
+  },
+  registerUser: async (req, res) => {
+    const { username, password, email } = req.body;
+    if (!username || !password || !email) {
+      return res.json({
+        success: false,
+        message: "sign the username, password or email",
+      });
+    }
+    try {
+      const FoundUser = await User.findOne({ username });
+      console.log(FoundUser);
+      if (!FoundUser) {
+        return res.json({
+          success: false,
+          message: "please choose another name",
+        });
+      }
+      const NewUser = new User({
+        username,
+        password,
+        email,
+      });
+      await NewUser.save();
+    } catch (error) {}
   },
 };
 
--- routers/AuthRouter.js
+++ routers/AuthRouter.js
@@ -3,5 +3,6 @@ const AuthController = require("../controllers/AuthController");
 const router = express.Router();
 
 router.post("/login", AuthController.loginUser);
+router.post("/register", AuthController.registerUser);
 
 module.exports = router;
