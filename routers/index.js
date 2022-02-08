419d79b9eca319215af87396d7f1b91587cfaeb4
shidomnt <hatranquang02@gmail.com>
2022-02-09 00:37:33 +0700

    add verify

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,13 @@
 const AuthRouter = require("./AuthRouter");
 const RoomRouter = require("./RoomRouter");
 const MessageRouter = require("./MessageRouter");
-
+const verify = require("../middlewares/auth");
 
 const router = (app) => {
 
   app.use("/api/users/", AuthRouter);
 
-  app.use("/api/rooms/", RoomRouter);
+  app.use("/api/rooms/",verify ,RoomRouter);
 
   app.use("/api/messages/", MessageRouter);
 
