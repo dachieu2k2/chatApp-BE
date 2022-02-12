1aa3f2a1bfa4b3becb0b3c30310a827ee1655fc4
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-12 22:33:50 +0700

    realtime message

--- config/io/index.js
+++ config/io/index.js
@@ -1,5 +1,5 @@
 const { Server } = require("socket.io");
-const { User } = require("../../models")
+const { User } = require("../../models");
 const http = require("http");
 
 const connect = (app) => {
@@ -9,32 +9,33 @@ const connect = (app) => {
   io.on("connection", (socket) => {
     console.log("Connected ", socket.id);
 
-    socket.on('leave room', ({ roomId }) => {
+    socket.on("leave room", ({ roomId }) => {
       socket.leave(roomId);
       console.log(`${socket.id} leave room ${roomId}`);
-    })
+    });
 
-    socket.on('join room', ({ roomId }) => {
+    socket.on("join room", ({ roomId }) => {
       socket.join(roomId);
       console.log(`${socket.id} join room ${roomId}`);
-    })
+    });
 
-    socket.on('create room', ({ username, ...rest }) => {
-      User.findOne({ username })
-        .then(user => {
-          socket.broadcast.emit('update room', user._id, rest);
-        })
-    })
+    socket.on("create room", ({ username, ...rest }) => {
+      User.findOne({ username }).then((user) => {
+        socket.broadcast.emit("update room", user._id, rest);
+      });
+    });
+    socket.on("create message", ({ roomId, ...action }) => {
+      socket.to(roomId).emit("update message", action);
+    });
 
     io.on("disconnect", () => {
       console.log("Disconnect ", socket.id);
-    })
-  })
+    });
+  });
 
   return server;
-}
+};
 
 module.exports = {
-  connect
-}
-
+  connect,
+};
