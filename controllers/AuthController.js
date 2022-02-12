6e19a09d3e1d3d742595229d7810d7c88ad1253e
dachieu2k2 <hieutedam2k2@gmail.com>
2022-02-12 23:45:19 +0700

    deploy again

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -62,7 +62,7 @@ const AuthController = {
         { userId: user._id },
         process.env.MY_SECRET_TOKEN,
         {
-          expiresIn: "60s",
+          expiresIn: "60d",
         }
       );
       const refreshToken = jwt.sign(
@@ -119,7 +119,7 @@ const AuthController = {
         { userId: NewUser._id },
         process.env.MY_SECRET_TOKEN,
         {
-          expiresIn: "60s",
+          expiresIn: "60d",
         }
       );
       return res.json({
