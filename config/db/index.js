d506375c2388abf4ee2ddabfdef0c6d8d47431ae
shidomnt <hatranquang02@gmail.com>
2022-02-17 21:56:36 +0700

    up

--- config/db/index.js
+++ config/db/index.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
     await mongoose.connect(
-      process.env.DB_URL
+      `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.ppm9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     );
     console.log(`connected DB`);
   } catch (error) {
@@ -14,4 +14,4 @@ const connect = async () => {
 
 module.exports = {
   connect,
-}
+};
