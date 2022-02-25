518c63599d267a9ae2b4d4eae62b07ef70791248
shidomnt <hatranquang02@gmail.com>
2022-02-25 14:56:20 +0700

    fix message

--- models/Room.js
+++ models/Room.js
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const MessageSchema = new Schema({
-  content: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-  roomId: {
-    type: String,
-    required: true,
-  }
-}, {
-  timestamps: true,
-})
-
-const RoomSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  messageList: {
-    type: [MessageSchema],
-    default: [],
-  }
-}, {
-=======
-const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
 const RoomSchema = new Schema(
@@ -46,7 +13,6 @@ const RoomSchema = new Schema(
     }
   },
   {
->>>>>>> 909827b276159a5f0da32721635420c9f1273e7d
     timestamps: true,
   }
 );
